b9bb6e341c908f74902ed50ba059e52c41c65531
Julian Perez <nemesis__13@hotmail.com>
2020-07-23 17:38:23 -0500

    Create Model and Context

---
+++ Shared/PizzaSpecial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazingPizza.Shared
+{
+    public class PizzaSpecial
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal BasePrice { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string GetFormattedBasePrice() => BasePrice.ToString("0.00");
+
+    }
+}
