db36652abc57f86f154c0be0540a7a0182d0290a
Azpel13 <nemesis__13@hotmail.com>
2020-07-28 13:54:20 -0500

    Modify data context

--- Server/Models/PizzaStoreContext.cs
+++ Server/Models/PizzaStoreContext.cs
@@ -10,10 +10,27 @@ namespace BlazingPizza.Server.Models
     public class PizzaStoreContext : DbContext
     {
         public DbSet<PizzaSpecial> Specials { get; set; }
+        public DbSet<Topping> Toppings { get; set; }
+        public DbSet<Pizza> Pizzas { get; set; }
+
 
         public PizzaStoreContext(
             DbContextOptions<PizzaStoreContext>options) 
             : base(options) { }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Definir la llave primaria de la entidad PizzaTopping
+            modelBuilder.Entity<PizzaTopping>()
+                .HasKey(pst => new { pst.PizzaId, pst.ToppingId });
+
+            // Una Pizza puede tener muchos Toppings.
+            modelBuilder.Entity<PizzaTopping>()
+                .HasOne<Pizza>().WithMany(ps => ps.Toppings);
+
+            // Un Topping puede estar en muchas Pizzas.
+            modelBuilder.Entity<PizzaTopping>()
+                .HasOne(pst => pst.Topping).WithMany();
+        }
     }
 }
