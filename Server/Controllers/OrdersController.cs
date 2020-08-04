ab113b6036e5b994829cbcfa2c39f1e808d9eeb0
Azpel13 <nemesis__13@hotmail.com>
2020-08-04 12:01:19 -0500

    Create API orders

---
+++ Server/Controllers/OrdersController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BlazingPizza.Server.Models;
+using BlazingPizza.Shared;
+
+namespace BlazingPizza.Server.Controllers
+{
+    [Route("orders")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly PizzaStoreContext Context;
+
+        public OrdersController(PizzaStoreContext context)
+        {
+            Context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> PlaceOrder(Order order)
+        {
+            order.CreatedTime = DateTime.Now;
+            //Establece una ubicacion de envio ficticia
+            order.DeliveryLocation= 
+                new LatLong(19.043679206924864, -98.19811254438645);
+            // Establecer el valor de Pizza.SpecialId y Topping.ToppingId
+            // para que no se creen nuevos registros Special y Topping.
+            foreach(var pizza in order.Pizzas)
+            {
+                pizza.SpecialId = pizza.Special.Id;
+                pizza.Special = null;
+                foreach(var topping in pizza.Toppings)
+                {
+                    topping.ToppingId = topping.Topping.Id;
+                    topping.Topping = null;
+                }
+            }
+
+            Context.Orders.Attach(order);
+            await Context.SaveChangesAsync();
+            return order.OrderId;            
+        }
+    }
+}
