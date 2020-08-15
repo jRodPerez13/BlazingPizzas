76cbcd14049b1e59c44af18f0b7f3b14d42b1603
Jr13 <Kamikaze__13@hotmail.com>
2020-08-14 20:44:50 -0500

    Add server-side validation

--- Shared/Address.cs
+++ Shared/Address.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazingPizza.Shared
 {
     public class Address
     {
         public int Id { get; set; }
+        [Required, MaxLength(100)]
         public string Name { get; set; }
+        [Required,MaxLength(100)]
         public string Line1 { get; set; }
+        [MaxLength(100)]
         public string Line2 { get; set; }
+        [Required,MaxLength(50)]
         public string City { get; set; }
+        [Required,MaxLength(20)]
         public string Region { get; set; }
+        [Required,MaxLength(20)]
         public string Postalcode { get; set; }
     }
 }
