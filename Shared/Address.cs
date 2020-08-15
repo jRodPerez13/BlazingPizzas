00fdb9f43686a73b1ee8f3844d68a4a4ae114751
Jr13 <Kamikaze__13@hotmail.com>
2020-08-14 21:47:51 -0500

    Using validation message

--- Shared/Address.cs
+++ Shared/Address.cs
@@ -9,22 +9,28 @@ namespace BlazingPizza.Shared
     {
         public int Id { get; set; }
 
-        [Required, MaxLength(100)]
+        [Required(ErrorMessage = "¿Quién recibirá la orden?"),
+            MaxLength(100)]
         public string Name { get; set; }
 
-        [Required,MaxLength(100)]
+        [Required(ErrorMessage = "Es necesario especificar la dirección de envío"),
+            MaxLength(100)]
+
         public string Line1 { get; set; }
 
-        [MaxLength(100)]
+        [MaxLength(100, ErrorMessage = "No se puede exceder a 100 caracteres")]
         public string Line2 { get; set; }
 
-        [Required,MaxLength(50)]
+        [Required(ErrorMessage = "La ciudad debe ser especificada"),
+            MaxLength(50)]
         public string City { get; set; }
 
-        [Required,MaxLength(20)]
+        [Required(ErrorMessage = "El estado debe ser especificado"),
+            MaxLength(20)]
         public string Region { get; set; }
 
-        [Required,MaxLength(20)]
+        [Required(ErrorMessage = "El código postal debe ser especificado"),
+            MaxLength(20)]
         public string Postalcode { get; set; }
     }
 }
