49cac2a980a2d66e82ce28b3093098648f048f10
Azpel13 <nemesis__13@hotmail.com>
2020-08-15 23:16:01 -0500

    Revert "Status order to master (#8)"
    This reverts commit 3a33dd94518d794085e70f09ec93b4dc5e2590ab.

--- Client/Program.cs
+++ Client/Program.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using BlazingPizza.Client.Services;
 
 namespace BlazingPizza.Client
 {
@@ -20,7 +19,7 @@ namespace BlazingPizza.Client
 
             builder.Services.AddScoped(sp => new HttpClient { 
                 BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddScoped<OrderState>();//AddScope para el actual usuario
+
             await builder.Build().RunAsync();
         }
     }
