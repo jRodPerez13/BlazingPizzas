4c7024f494d7bf64f4dd57f39ede02603378ab26
Jr13 <Kamikaze__13@hotmail.com>
2020-08-20 19:44:50 -0500

    Add a confirmation message when removing pizzas

---
+++ Client/Services/JSRuntimeExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace BlazingPizza.Client.Services
+{
+    public static class JSRuntimeExtension
+    {
+       public static ValueTask<bool> Confirm(this IJSRuntime jSRuntime, 
+           string message)
+        {
+            return jSRuntime.InvokeAsync<bool>("confirm", message);
+        }
+    }
+}
