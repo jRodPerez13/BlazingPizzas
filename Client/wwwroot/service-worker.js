4de2c47def484192ab3d1cf70a1fa546d2914e67
Jr13 <Kamikaze__13@hotmail.com>
2020-08-24 14:08:02 -0500

    Make app install

--- Client/wwwroot/service-worker.js
+++ Client/wwwroot/service-worker.js
@@ -2,4 +2,13 @@
 self.addEventListener('install', async event => {
     console.log('Instalando el Service Worker...');
     self.skipWaiting();
-})
+});
+
+self.addEventListener('fetch', event => {
+    // Podemos agregar aquí lógica personalizada para controlar
+    // si se pueden utilizar los datos en caché cuando la aplicación
+    // se ejecuta fuera de línea.
+    // La siguiente línea hace que las solicitudes vayan directamente
+    // a la red como de costumbre.
+    return null
+});
